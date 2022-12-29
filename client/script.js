a6fb0b9969121b32de0d17aebe575edb8318df4b
northmsaad <msddr010@gmail.com>
2022-12-29 23:01:18 +0500

    final

--- client/script.js
+++ client/script.js
@@ -10,10 +10,8 @@ function loader(element) {
   element.textContent = "";
 
   loadInterval = setInterval(() => {
-    // Update the text content of the loading indicator
     element.textContent += ".";
 
-    // If the loading indicator has reached three dots, reset it
     if (element.textContent === "....") {
       element.textContent = "";
     }
@@ -33,9 +31,6 @@ function typeText(element, text) {
   }, 20);
 }
 
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
 function generateUniqueId() {
   const timestamp = Date.now();
   const randomNumber = Math.random();
@@ -65,25 +60,45 @@ const handleSubmit = async (e) => {
 
   const data = new FormData(form);
 
-  // user's chatstripe
   chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
 
-  // to clear the textarea input
   form.reset();
 
-  // bot's chatstripe
   const uniqueId = generateUniqueId();
   chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
 
-  // to focus scroll to the bottom
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
-  // specific message div
   const messageDiv = document.getElementById(uniqueId);
 
-  // messageDiv.innerHTML = "..."
   loader(messageDiv);
+
+  const response = await fetch("http://localhost:5000/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = " ";
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
 };
+
 form.addEventListener("submit", handleSubmit);
 form.addEventListener("keyup", (e) => {
   if (e.keyCode === 13) {
